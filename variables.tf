405d599246c33501abd6c2d58e231c26f93df61e
TarasLeto <taras546@i.ua>
2022-08-19 19:23:00 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "my-access-key" {
-    type = "AKIATZGP3ZVGBPJN4D6Q"
+    type= string
+	default = "AKIATZGP3ZVGBPJN4D6Q"
 }
 variable "my-secret-key" {
-    type = "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"
+	type= string
+   default = "qMhqFMrgl7hEdFug2rYD7EjaUjuZh60u2zjTh7UE"
 	}
 	
