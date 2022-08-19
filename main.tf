9348a6b22baf49e33fc51e01dd1c3885c971e1f7
TarasLeto <taras546@i.ua>
2022-08-19 18:49:40 +0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.5"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  }
+  
+  
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "access_key" {
+    type = "AKIATZGP3ZVGBPJN4D6Q"
+}
+variable "secret_key" {
+    type = "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"
+	}
+	
