44847ea714f3a347c8b6490edcd4e4c694f81145
TarasLeto <taras546@i.ua>
2022-10-05 21:37:35 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+provider "aws" {
+  region  = "us-east-1"
+}
 
 resource "aws_instance" "webserver1" {
 
