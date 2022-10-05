51c8032580e61b9ca4415598f96b6f4fc32228ca
TarasLeto <taras546@i.ua>
2022-10-05 18:33:26 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,4 +6,20 @@ variable "my-secret-key" {
 	type= string
    default = "qMhqFMrgl7hEdFug2rYD7EjaUjuZh60u2zjTh7UE"
 	}
-	
+variable "image_id" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+	default= "ami-0149b2da6ceec4bb0 "
+}
+variable "instance_type" {
+type = string 
+	default ="t2.micro"
+}
+variable "key" {
+type = string 
+default ="my_terraform_key"
+}
+variable "privatekeypath" {
+	type = string
+	default ("./terrafrom-jenkins/my_terraform_key.pem")
+}
