7a1a40176b17ba56baeb9d688748ad3b6fc45845
TarasLeto <taras546@i.ua>
2022-10-05 18:40:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 
-resource "aws_instance" "appserver1" {
+resource "aws_instance" "webserver1" {
 
-  ami             = var.imageid
-  instance_type   = var.instancetype
+  ami             = var.image_id
+  instance_type   = var.instance_type
   key_name        = var.key
-  security_groups = [aws_security_group.allow_all.name]
+  vpc_security_group_ids = [aws_security_group.my-sg.id]
+  associate_public_ip_address = true
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "8"
+    delete_on_termination = true
 
-
-  provisioner "remote-exec" {
+  }
+}
+provisioner "remote-exec" {
 
     connection {
        type        = "ssh"
@@ -15,11 +21,22 @@ resource "aws_instance" "appserver1" {
        private_key = file(var.privatekeypath)
        host        = self.public_ip
     }
+       inline = [
+      "sudo apt-get update", 
+      "sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release",
 
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install tomcat7 -y"
-    ]
+  "sudo mkdir -p /etc/apt/keyrings",
+  "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+  "sudo apt-get update",
+  "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+ "sudo service docker start",
+ "sudo docker pull tarasleto96/gs-rest_service:lts",
+ "sudo docker run -d -p 8080:8080  tarasleto96/gs-rest_service:lts"
+]
   }
 }
   
