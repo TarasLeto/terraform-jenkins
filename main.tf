14ca3f1ed1b59fc7cb3a5e2da1d9534a745e8309
TarasLeto <taras546@i.ua>
2022-10-06 16:51:25 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,19 +31,19 @@ resource "aws_instance" "webserver1" {
  connection {
        type        = "ssh"
        user        = "ubuntu"
-       private_key = file(var.privatekeypath)
+       private_key = file("/my_terraform_key.pem")
        host        = self.public_ip
     }
  
    provisioner "remote-exec" {
        inline = [
       "sudo apt-get update -y", 
-      "sudo apt-get install ca-certificates & curl & gnupg & lsb-release",
+      "sudo apt-get install ca-certificates & curl & gnupg & lsb-release -y",
 
   "sudo mkdir -p /etc/apt/keyrings",
   "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
-  "sudo apt-get update",
-  "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+  "sudo apt-get update -y",
+  "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y",
  "sudo service docker start",
  "sudo docker pull tarasleto96/gs-rest_service:lts",
  "sudo docker run -d -p 8080:8080  tarasleto96/gs-rest_service:lts"
