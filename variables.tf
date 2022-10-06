1fd02ed9bdc629d20cf85ace4032ec0199311255
TarasLeto <taras546@i.ua>
2022-10-06 17:35:21 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ default ="my_terraform_key"
 }
 variable "privatekeyvalue" {
 	type = string
-	default = <<EOF 
+	default = <<-EOF 
 	-----BEGIN RSA PRIVATE KEY-----
 MIIEpAIBAAKCAQEArzZM15ZaTpswLvo/gxoHoM5lU6Et1O7dP8w7xUPLxXLRXpcs
 L6wGpPWbCTqaelCOEgsIYtkFm3DqYy4XrQ6BQzV6gwVw/KRx0a92saHc/DF88F8A
