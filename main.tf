39b946c418568ccb9ba88812ccf3f466ea0d4e76
TarasLeto <taras546@i.ua>
2022-10-06 17:21:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "webserver1" {
  connection {
        type        = "ssh"
        user        = "ubuntu"
-       private_key = file("var.privatekeypath")
+       private_key = var.privatekeyvalue
        host        = self.public_ip
     }
    provisioner "remote-exec" {
