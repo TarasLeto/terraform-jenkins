5c678070f4ceb66a3fc230b8fbb89ee529aaa088
TarasLeto <taras546@i.ua>
2022-10-11 18:16:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "webserver1" {
  connection {
        type        = "ssh"
        user        = "ubuntu"
-       private_key = file("${path.module}/my_terraform_key")
+       private_key = var.privatekeyvalue
        host        = self.public_ip
     }
    provisioner "remote-exec" {
